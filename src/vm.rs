50e2be4ebe2b25a844aed0afa2234dbf3ed151b0
RNGKing <pxp.phillips@gmail.com>
2021-02-24 22:00:20 -0600

    Added simplistic branching to work loop

--- src/vm.rs
+++ src/vm.rs
@@ -11,26 +11,24 @@ struct VirtualMachine {
 }
 
 impl VirtualMachine {
-    pub fn run(&self){
-        loop {
-            // READ INPUTS AND ASSIGN THEIR VALUES TO REGISTERS
-            for collection in self.rungs.iter() {
-                let mut index = 0;
-                while index < collection.get_len(){
-                    let inst = collection.get_instruction(index); 
-                    if inst.get_opcode().is_boolean() {
-                        // Evaluate boolean 
-                        index = VirtualMachine::EvaluateBooleanStatement(&inst, inst.get_opcode().boolean(&inst, &self.registers), index);
-                    }
-                    else{
-                        // Evaluate operation
-
-                    }
+    pub fn run(&mut self){
+        // READ INPUTS AND ASSIGN THEIR VALUES TO REGISTERS
+        for collection in self.rungs.iter() {
+            let mut index = 0;
+            while index < collection.get_len(){
+                let inst = collection.get_instruction(index); 
+                if inst.get_opcode().is_boolean() {
+                    // Evaluate boolean 
+                    index = VirtualMachine::EvaluateBooleanStatement(&inst, inst.get_opcode().boolean(&inst, &self.registers), index);
+                }
+                else{
+                    // Evaluate operation
+                    self.registers = inst.get_opcode().execute(&inst, &self.registers);
                 }
             }
-            // READ OUTPUT REGISTERS AND WRITE THEIR VALUES TO PINS
-            println!("Ran a successful loop");
         }
+        // READ OUTPUT REGISTERS AND WRITE THEIR VALUES TO PINS
+        println!("Ran a successful loop");
     }
 
     fn EvaluateBooleanStatement(inst : &InstructionData, truth_value : bool, curr_index : usize) -> usize {
